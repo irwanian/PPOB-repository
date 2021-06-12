4f13f82fd33f10e23e4d552f5762542fc6ba550c
Irwan Ramadhan <irwan.ramadhan@modalrakyat.id>
2021-06-12 21:18:34 +0700

    implement auth middleware

--- src/transformers/ppob_transaction/list_by_id.js
+++ src/transformers/ppob_transaction/list_by_id.js
@@ -1,3 +1,19 @@
+const getDestinationNumber = (destination_number, provider) => {
+    let result
+    
+    if (provider === 'bpjs') {
+        result = destination_number.split(':')[0]
+    } else if (provider === 'pdam') {
+        result = destination_number.split(':')[1]
+    } else {
+        result = destination_number
+    }
+
+    return result
+}
+
+
+
 module.exports = {
     transform: (payloads) => {
         return payloads.map(payload => {
@@ -5,7 +21,7 @@ module.exports = {
                 id: payload.id,
                 product: payload.pp_name.includes('POSTPAID') ? payload.pp_name.replace('POSTPAID', 'Pasca Bayar') : payload.pp_name,
                 provider: payload.pp_provider,
-                destination_number: payload.destination_number.includes(':') ? payload.destination_number.split(':')[1] : payload.destination_number,
+                destination_number: getDestinationNumber(payload.destination_number, payload.provider.toLowerCase()),
                 category: payload.pp_category,
                 transaction_status: payload.status,
                 payment_status: payload.pa_status,
