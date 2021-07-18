a9c8fa5bc786c42f277cb6d19690e1ceee414ea0
Irwan Ramadhan <irwan.ramadhan@modalrakyat.id>
2021-07-18 11:05:10 +0700

    test tyransaction advice

--- src/methods/ppob/test_transaction_advice.js
+++ src/methods/ppob/test_transaction_advice.js
@@ -4,21 +4,14 @@ const Helpers = require('../../utils/helpers')
 const PpobService = require('../../services/ppob')
 
 module.exports = testPpobPrepaid = async (req, res) => {
-    const { user_id, product_id, destination_number } = req.body
+    const { product_id, msisdn } = req.body
     
     try {
         let products = await PpobRepository.findOne({ id: product_id })
         products = Helpers.parseDataObject(products)
-        const user = {
-            name: req.session.name,
-            email: req.session.email,
-            phone: req.session.phone
-        }
-
-        const { product_code, msisdn  } = req.body
-
+        
         const transactionPayload = {
-            product_code,
+            product_code: products.code.split('-')[1],
             msisdn
         }
 
